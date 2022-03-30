56c3aa890623ef473bebd5c535c4849f53b15188
Henk Beula <henkbeula82@gmail.com>
2022-03-30 11:12:10 +0200

    Add files via upload

---
+++ Exercices/Main.java
@@ -0,0 +1,172 @@
+public class Main{
+    public static String move2end(java.lang.String str, java.lang.Integer n) {
+        if(str.length()<3)
+            return str;
+        else
+            return str.substring(n, str.length()) + str.substring(0, n);    
+    }
+
+    public static String cutMiddle(String s){
+        if(s.length() % 2 == 0){
+            return s.substring(0, (s.length()/2)-1) + s.substring((s.length()/2)+1, s.length());
+        }
+        return s.substring(0, (s.length()/2)) + s.substring((s.length()/2)+1, s.length());
+    }
+
+    public static String doubleChar(String s){
+        String result = "";
+        char[] chars = s.toCharArray();
+        for(int i = 0; i < s.length(); i++){
+            result += chars[i];
+            result += chars[i];
+        }
+         return result;
+     }
+     
+    public static int countNumChar(String s, char ch){
+        int repetitions =0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if(chars[i]==ch)
+                repetitions++;
+        }
+        return repetitions;
+    }      
+
+    /*public static int countNumString(String s, String find){
+        int v =0;
+        if(s.contains(find)){
+            System.out.println(v++);
+        }
+        return 0;
+    }*/
+    
+    public static String everyOtherChar0(String s){
+        char[] chars = s.toCharArray();
+        String result = "";
+
+        for (int i = 0; i < chars.length; i+=2) {
+            result += chars[i];
+        }
+        return result;
+    }     
+
+    public static int countCharsAfterFirst(String s, char ch1, char ch2){
+        int repetitions =0, firstOccor=0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if(chars[i]==ch1){
+                repetitions++;
+                firstOccor=i;
+                break;
+            }
+        }
+        if(repetitions>0){
+            repetitions=0;
+            for (int i = firstOccor; i < chars.length; i++) {
+                if(chars[i]==ch2){
+                    repetitions++;
+                }
+            }
+            return repetitions;
+        }
+        return 0;
+    }
+
+
+    public static int countCharsOutsideRange(String s, char ch, int start, int end){
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(i>=start && i<end)
+                continue;
+            else if(s.charAt(i)==ch)
+                count++;
+        }
+        return count;
+    }
+    
+    public static int countCharsInRange(String s, char ch, int start, int end){
+        int count = 0;
+        for (int i = start; i < end && i<s.length(); i++) {
+            if(i>=start && i<end && s.charAt(i)==ch)
+                count++;
+        }
+        return count;
+    }
+    
+    public static int countA(String s){
+        int count=0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)=='a')
+                count++;
+        }return count;
+    }
+
+    public static String reverse(String s){
+        if(s.length()==1)
+            return s;
+        return reverse(s.charAt(s.length()-1));
+    }
+
+    /*public static boolean containsNM(int[] arr, int n, int m){
+        boolean containsN  = false, containsM = false;
+        for (int i : arr) {
+            if(i==n)
+                containsN=true;
+            if(i==m)
+                containsM=true;
+        }
+        return (containsM && containsN);
+    }*/
+
+    public static boolean containsNM(int[] arr, int n, int m){
+        boolean contains = false, containsN  = false, containsM = false;
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i]==n && arr[i+1]==m || arr[i]==m && arr[i+1]==n){
+                return false;
+            }
+        }
+        for (int i : arr) {
+            if(i==n)
+                containsN=true;
+            if(i==m)
+                containsM=true;
+        }
+        return (containsM && containsN);
+    }
+
+    public static int findMin(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for(int elmt : arr) {
+            if(elmt < min) {
+                min = elmt;
+            }
+        }
+        return min;
+    }
+
+    public static int minIndex(int[] arr){
+        int min = Integer.MAX_VALUE, minIndex=0, index=0;
+        while(index < arr.length) {
+            if(arr[index] < min) {
+                min=arr[index];
+                minIndex = index;
+                //break;
+            }
+            index++;
+        }
+        return minIndex;
+    }
+    
+    public static int findSum(int[] arr) {
+        int sum = 0;
+        for(int elmt : arr){
+            sum += elmt;
+        }
+        return sum;
+    }
+    
+    public static boolean lessThan10(int n){
+        return (n < 10) ? true: false;
+    }
+}
